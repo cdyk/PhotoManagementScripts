91a1e19796fdd6531c7d49c242d9965f13e1c2a7
Christopher Dyken <cdyk@server.fake>
2014-03-12 22:53:49 +0100

    cleanup

--- fspot2xmp.pl
+++ fspot2xmp.pl
@@ -6,9 +6,9 @@ use Image::ExifTool qw(:Public);
 use URI::Escape;
 use Encode qw(decode encode);
 use Encode::Guess;
-use 5.010;      # we use smart match
 
 my $dryrun            = 1;
+my $dump_xmp          = 0;
 my $remove_jpg_if_cr2 = 1; 
 my $photos_processed  = 0;
 my $tag_encoding      = "Guess";
@@ -18,6 +18,7 @@ sub print_help {
         print STDERR "Recognized options:\n";
         print STDERR "    --help               This help\n";
         print STDERR "    --dryrun             Don't actually write anything.\n";
+        print STDERR "    --no-dryrun          Actually do stuff.\n";
         print STDERR "    --db path            Specify path to F-Spot database.\n";
         print STDERR "    --remove_jpg_if_cr2  Remove jpg image if cr2 version exists.\n";
 }
@@ -36,6 +37,9 @@ while( my $arg = shift @ARGV ) {
     elsif( $arg eq "--dryrun" ) {
         $dryrun = 1;
     }
+    elsif( $arg eq "--no-dryrun" ) {
+        $dryrun = 0;
+    }
     elsif( $arg eq "--remove_jpg_if_cr2" ) {
         $remove_jpg_if_cr2 = 1;
     }
@@ -46,23 +50,10 @@ while( my $arg = shift @ARGV ) {
     }
 }
 
-# Check if db file exist
-unless( -e $db_path ) {
-    print "Db path '$db_path' does not exist.\n";
-    die;
-}
-
-# Check if SQLite is available
-unless( "SQLite" ~~ [DBI->available_drivers()] ) {
-    print "DBI is missing the SQLite driver, available drivers:\n";
-    print join("\n", DBI->available_drivers()), "\n";
-    die;
-}
-
+# --- Connect to database ------------------------------------------------------
 my $dbh = DBI->connect( "dbi:SQLite:$db_path" ) or die $DBI::errstr;
 
-
-# --- Create tag hierarchies -------------------------------------------------
+# --- Create tag hierarchies ---------------------------------------------------
 my %tags;
 {   
     my $sth = $dbh->prepare( "SELECT id, name, category_id FROM tags" ) or die $DBI::errstr;
@@ -86,33 +77,39 @@ my %tags;
     }
 }
 
-# --- Plow through photos ----------------------------------------------------
+# --- Plow through photos ------------------------------------------------------
 {   
     my $sth = $dbh->prepare( "SELECT id, base_uri, filename FROM photos" ) or die $DBI::errstr;
     $sth->execute();
     while( my ($id, $base_uri, $filename ) = $sth->fetchrow_array() ) {
 
-        # --- get versions of this photo -------------------------------------
+
+        # ======================================================================
+        # ===                                                                ===
+        # === Determine all versions of this photo                           ===
+        # ===                                                                ===
+        # ======================================================================
+
         my $versions = $dbh->selectall_arrayref( "SELECT name, base_uri, filename ".
                                                  "FROM photo_versions ".
                                                  "WHERE photo_id='$id'".
                                                  "ORDER BY name DESC" )  or die $DBI::errstr;
 
-        # --- If versions was empty, add the default from the photos table ---
+        # --- If versions was empty, add the default from the photos table -----
         unless( @{$versions} ) {
             $versions = [ ['Default', $base_uri, $filename] ];
             print STDERR "[ID='$id'] No version info, reverting to default.\n";
         }
 
-        # --- Get full paths and filter out nonexistent files ----------------
+        # --- Clean up paths and discard non-existing files --------------------
         $versions = [grep {
+
             # Chop of file://-prefix and do url_decode to remove the %xx-stuff.
             $$_[3] = uri_unescape( substr( $$_[1], 7 )."/".$$_[2] );
+
             # Return true (keep) if file exist, otherwise false will remove it
             unless( -e $$_[3] ) {
-                print STDERR "[ID='$id'] Couldn't find file  with prefix '$$_[1]' and ".
-                             "filename '$$_[2]', ".
-                             "skipping version.\n";
+                print STDERR "[ID='$id'] Skipping non-existing file '$$_[3]'\n";
                 0;
             }
             else {
@@ -130,14 +127,16 @@ my %tags;
         if( ($remove_jpg_if_cr2)
             && (@{$versions} == 2)
             && ($$versions[0][0] eq 'Original') && (lc(substr($$versions[0][2],-4)) eq ".cr2")
-            && ($$versions[1][0] eq 'Jpeg') && ( (lc(substr($$versions[1][2],-4)) eq ".jpg")
-                                              || (lc(substr($$versions[1][2],-5)) eq ".jpeg") ) )
+            && ($$versions[1][0] eq 'Jpeg')     && ( (lc(substr($$versions[1][2],-4)) eq ".jpg")
+                                                  || (lc(substr($$versions[1][2],-5)) eq ".jpeg") ) )
         {
-            if( $dryrun || 1 ) {
-                print STDERR "[ID='$id'] Would have deleted file '", $$versions[1][3], "'\n";
+            my @files = glob "$$versions[1][3]*";
+
+            foreach my $item (@files) {
+                print STDERR "[ID='$id'] Unlinking duplicate '$item'\n";
             }
-            else {
-                # delete JPG and sidecar file if exists.
+            unless( $dryrun ) {
+#                unlink @files;
             }
             pop @{$versions};
         }
@@ -156,17 +155,13 @@ my %tags;
             push @{$lr_tags}, join('|',@{$tags{$tag_id}[0]});
         }
 
-        # --- Create XMP data structure --------------------------------------
-        foreach my $version (@{$versions}) {
-            my $xmpfilename = undef;
-            if( -e $$version[3].".xmp" ) {
-                $xmpfilename = $$version[3].".xmp";
-            }
-            elsif( -e $$version[3].".XMP" ) {
-                $xmpfilename = $$version[3].".XMP";
-            }
+        # ======================================================================
+        # ===                                                                ===
+        # === Create or update XMP data                                      ===
+        # ===                                                                ===
+        # ======================================================================
 
-            print $$version[3],"\n";
+        foreach my $version (@{$versions}) {
 
             my $exifTool = new Image::ExifTool;
 
@@ -176,32 +171,43 @@ my %tags;
             # Adobe lightroom tags
             $exifTool->SetNewValue( 'HierarchicalSubject' => $lr_tags ) or die $exifTool->GetValue('Error');
 
+            # --- check if we have an existing XMP file ------------------------
+            my $xmpfilename = undef;
+            if( -e $$version[3].".xmp" ) {
+                $xmpfilename = $$version[3].".xmp";
+            }
+            elsif( -e $$version[3].".XMP" ) {
+                $xmpfilename = $$version[3].".XMP";
+            }
             # --- XMP file already exist, merge ------------------------------
             if( $xmpfilename ) {
-                if( $dryrun  ) {
-                    print STDERR "[ID='$id'] Would have updated  '",$xmpfilename,"'\n";
+                print STDERR "[ID='$id'] Updating existing XMP file '$xmpfilename'\n";
+                if( $dump_xmp ) {
+                    $exifTool->WriteInfo( $xmpfilename, '-', 'XMP' ) or die $exifTool->GetValue('Error');
                 }
-                else {
+                unless( $dryrun ) {
                     #$exifTool->WriteInfo( $xmpfilename ) or die $exifTool->GetValue('Error');
                 }
             }
             # --- No XMP file, create new ------------------------------------
             else {
                 $xmpfilename = $$version[3].".xmp";
-                if( $dryrun  ) {
-                    print STDERR "[ID='$id'] Would have created new file '",$xmpfilename,"'\n";
+                print STDERR "[ID='$id'] Creating new XMP file '$xmpfilename'\n";
+
+                if( $dump_xmp ) {
                     $exifTool->WriteInfo( undef, '-', 'XMP' ) or die $exifTool->GetValue('Error');
                 }
-                else {
+
+                unless( $dryrun ) {
                     #$exifTool->WriteInfo( undef, $xmpfilename, 'XMP' ) or die $exifTool->GetValue('Error');
                 }
             }
         }
 
         $photos_processed++;
-        if( $photos_processed > 200 ) {
-            last;
-        }
+        #if( $photos_processed > 200 ) {
+        #    last;
+        #}
     }
     $sth->finish();
 }
